4d311a56743e6ce4b975ea5c32bbf39438ac8347
Eng-Phenomena <eng.phenomena@gmail.com>
2024-07-26 13:54:48 +0000

    fin

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ALX_Interview Question
+Pascal Triangle
+"""
+def pascal_triangle(n):
+    main_list = []
+    if (n <= 0):
+        return (main_list)
+    elif (n == 1):
+        main_list.append([1])
+        return (main_list)
+    elif (n == 2):
+        main_list.append([1])
+        main_list.append([1,1])
+        return main_list
+    else:
+        main_list.append([1])
+        main_list.append([1,1])
+        for i in range(2, n):
+            tmp = [1]
+            for j in range(i - 1):
+                val = main_list[i-1][j] + main_list[i-1][j+1]
+                tmp.append(val)
+                if ((j+1) == (i-1)):
+                    tmp.append(1)
+                    main_list.append(tmp)
+        return main_list
